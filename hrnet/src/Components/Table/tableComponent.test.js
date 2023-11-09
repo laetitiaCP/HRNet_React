4fe4fc56591aacce3ab1ef7f939c5edd23f28cc6
laetitiaCP <laetitia.cendra.pro@gmail.com>
2023-11-09 10:57:27 +0100

    <feat><finition des tests>

--- hrnet/src/Components/Table/tableComponent.test.js
+++ hrnet/src/Components/Table/tableComponent.test.js
@@ -1,22 +1,54 @@
-import {render} from "@testing-library/react";
+import {render, screen} from "@testing-library/react";
 import TableComponent from "./tableComponent";
 
 describe("<TableComponent />", () => {
-    test("Les colonnes sont bien présentes", () => {
-        const sortBy = jest.fn();
-        const handleHeader = jest.fn();
-        const sortDirection = jest.fn();
+    test("Le nombre de colonnes est correct", () => {
         const tableHeader = () => {
-            let headerElement = ["firstname", "lastname", "startDate", "Department", "dateOfBirth", "street", "City", "States", "zipCode"];
+            let headerElement = ["un", "deux", "trois", "quatre"];
             return headerElement.map((header) => {
-                return <th key={header}
-                           className={sortBy === header ? sortDirection ? "ascending-sort" : "descending-sort" : ""}>
-                    <a onClick={handleHeader}>
-                        {header}
-                    </a>
-                </th>
+                return <th key={header}>{header}</th>
             });
+        };
+        const dataBody = () => {
+            const listData = [{un:"premier", deux:"deuxième", trois:"troisième", quatre:"quatrième"}];
+            return listData.map(({un, deux, trois, quatre}, index) => {
+                return (
+                    <tr key={index}>
+                        <td>{un}</td>
+                        <td>{deux}</td>
+                        <td>{trois}</td>
+                        <td>{quatre}</td>
+                    </tr>
+                )
+            })
         }
-        render(<TableComponent dataHeader={tableHeader} dataBody={dataBody}/>)
+        const container = render(<TableComponent dataHeader={tableHeader} dataBody={dataBody}/>)
+
+        expect(container.getAllByRole("columnheader")).toHaveLength(4);
+    })
+
+    test("Le nombre de lignes est correct", () => {
+        const tableHeader = () => {
+            let headerElement = ["un", "deux", "trois", "quatre"];
+            return headerElement.map((header) => {
+                return <th key={header}>{header}</th>
+            });
+        };
+        const dataBody = () => {
+            const listData = [{un:"premier", deux:"deuxième", trois:"troisième", quatre:"quatrième"}];
+            return listData.map(({un, deux, trois, quatre}, index) => {
+                return (
+                    <tr key={index}>
+                        <td>{un}</td>
+                        <td>{deux}</td>
+                        <td>{trois}</td>
+                        <td>{quatre}</td>
+                    </tr>
+                )
+            })
+        }
+        const container = render(<TableComponent dataHeader={tableHeader} dataBody={dataBody}/>)
+
+        expect(container.getAllByRole("row")).toHaveLength(2); // header + data = 2 lignes
     })
 })
